6564c27a477f91b6b01980414a682640843cac8a
CJ_Paiva <carlos.alberto.2012@hotmail.com>
2020-05-23 14:47:24 -0300

    Aula 2 - Criando apps necessárias

--- aulaCanal/settings.py
+++ aulaCanal/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'aulaCanal.core',
+    'aulaCanal.cliente',
+    'aulaCanal.produto',
+    'aulaCanal.compra',
 ]
 
 MIDDLEWARE = [
